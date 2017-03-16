385cc4e59ae50b151e87d185baf6f8174348901c
Omar Suarez <vegetali@gmail.com>
2017-03-16 14:29:05 +0100

    All the files are added to the project. All works fine!

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'usb_test' 
+ * Target:  'usb_test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/usb_test/startup_stm32f769xx.d
@@ -0,0 +1,1 @@
+usb_test\startup_stm32f769xx.o: startup_stm32f769xx.s
